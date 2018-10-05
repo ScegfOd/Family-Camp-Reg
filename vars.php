9ce85dcea4492c6c57ac3e6e9673f6a87ffcb8c8
Jonathan Combs <ScegfOd@users.noreply.github.com>
2018-10-05 02:55:24 -0700

    Add files via upload

---
+++ vars.php
@@ -0,0 +1,22 @@
+<?php
+//see "/countdown/js/soon/custom.js" for the clock countdown
+
+$camp_title = "Bay Area Family Camp 2016";
+$camp_motto = "~meet God with your family in nature~";
+
+$camp_date = "Wednesday, August 17 – Sunday, August 21, 2016";
+
+
+$name_of_price = "EARLY BIRD DISCOUNT PRICES";
+//$name_of_price = "PRE CAMP PRICES"
+//$name_of_price = "JUST IN TIME PRICES"
+
+$date_of_price_expiration = "July 10th";
+
+$adult_fee = 42;
+$adolescent_fee = 35;
+$child_fee = 30;
+$fam_reg_fee = 20;
+$cabin_pp_pn_fee = 10;
+$bunkhouse_pp_pn_fee = 15;
+?>
